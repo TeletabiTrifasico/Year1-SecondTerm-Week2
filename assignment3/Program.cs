abf0ba6371dbca9b7f062ba9439aa43d5755f2d0
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 18:36:25 +0100

    Add files via upload

---
+++ assignment3/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+        void Start()
+        {
+            RegularCandies[,] playingField = new RegularCandies[10, 10];
+            InitCandies(playingField);
+            DisplayCandies(playingField);
+        }
+        void InitCandies(RegularCandies[,] candies)
+        {
+            Random random = new Random();
+            for (int i = 0; i < candies.GetLength(0); i++)
+            {
+                for (int j = 0; j < candies.GetLength(1); j++)
+                {
+                    candies[i, j] = (RegularCandies)random.Next(0, 6);
+                }
+            }
+        }
+        void DisplayCandies(RegularCandies[,] candies)
+        {
+            for (int i = 0; i < candies.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < candies.GetLength(1); j++)
+                {
+                    if (candies[i, j] == RegularCandies.JellyBean)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("# ");
+                        Console.ResetColor();
+                    }
+                    else if (candies[i, j] == RegularCandies.Lozenge)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("# ");
+                        Console.ResetColor();
+                    }
+                    else if (candies[i, j] == RegularCandies.LemonDrop)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("# ");
+                        Console.ResetColor();
+                    }
+                    else if (candies[i, j] == RegularCandies.GumSquare)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("# ");
+                        Console.ResetColor();
+                    }
+                    else if (candies[i, j] == RegularCandies.LollipopHead)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("# ");
+                        Console.ResetColor();
+                    }
+                    else if (candies[i, j] == RegularCandies.JujubeCluster)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write("# ");
+                        Console.ResetColor();
+                    }
+                }
+            }
+        }
+        bool ScoreRowPresent(RegularCandies[,] candies)
+        {
+            for (int i = 0; i < candies.GetLength(1); i++)
+            {
+                int counter = 1;
+                RegularCandies currentCandie = candies[i, 0];
+
+                for (int j = 0; j < candies.GetLength(0); j++)
+                {
+                    if (candies[i,j] == currentCandie)
+                    {
+                        counter++;
+                        if (counter >= 3)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            currentCandie = candies[i, j];
+                            counter = 1;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        bool ScoreColumnPresent(RegularCandies[,] candies)
+        {
+            for (int i = 0; i < candies.GetLength(1); i++)
+            {
+                int counter = 1;
+                RegularCandies currentCandie = candies[0, i];
+
+                for (int j = 0; j < candies.GetLength(0); j++)
+                {
+                    if (candies[i, j] == currentCandie)
+                    {
+                        counter++;
+                        if (counter >= 3)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            currentCandie = candies[i, j];
+                            counter = 1;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ assignment3/RegularCandies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    internal enum RegularCandies
+    {
+        JellyBean, //(red)
+        Lozenge, //(orange)
+        LemonDrop, //(yellow)
+        GumSquare, //(green)
+        LollipopHead, //(blue) 
+        JujubeCluster //(purple)
+    }
+}
